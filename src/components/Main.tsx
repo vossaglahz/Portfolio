679fafd2a4d95530305f549ae7f38c2e5ea25e46
Merey <crowtwentyohtwo@gmail.com>
2024-11-09 21:11:24 +0500

    Footer is set

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,11 @@
+import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
 
 export const Main = () => {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     )
 };
